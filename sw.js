60405d76cf9cf385f65ba8f4d69af8672474ae9f
Twinny Support <twinsxstore@gmail.com>
2022-03-02 00:24:40 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,68 @@
-var SLOW_TIME = 3000;
+const js = `
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+if (workbox) {
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+workbox.core.setCacheNameDetails({
+  prefix: 'thn-sw',
+  suffix: 'v22',
+  precache: 'install-time',
+  runtime: 'run-time'
+});
 
-this.addEventListener( 'install', function () {
-  console.log('Installed service worker');
-} );
+const FALLBACK_HTML_URL = '/offline.html';
+const version = workbox.core.cacheNames.suffix;
+workbox.precaching.precacheAndRoute([{url: FALLBACK_HTML_URL, revision: null},{url: '/manifest.json', revision: null},{url: '/favicon.ico', revision: null}]);
 
-this.addEventListener( 'fetch', function(event) {
-  var url = event.request.url;
+workbox.routing.setDefaultHandler(new workbox.strategies.NetworkOnly());
 
-  if ( url.indexOf( 'blocking' ) === -1) {
-  return;
+workbox.routing.registerRoute(
+    new RegExp('.(?:css|js|png|gif|jpg|svg|ico)$'),
+    new workbox.strategies.CacheFirst({
+        cacheName: 'images-js-css-' + version,
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxAgeSeconds: 60 * 24 * 60 * 60,
+                maxEntries:200,
+                purgeOnQuotaError: true
+            })
+        ],
+    }),'GET'
+);
+
+workbox.routing.setCatchHandler(({event}) => {
+      switch (event.request.destination) {
+        case 'document':
+        return caches.match(FALLBACK_HTML_URL);
+      break;
+      default:
+        return Response.error();
   }
+});
 
-  var promise = Promise.race( [
-    new Promise( ( resolve, reject) => setTimeout(
-      () => reject( new Response( 'Request killed!' ) ),
-      SLOW_TIME
-    ) ),
-    fetch( event.request ),
-  ] );
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches
+      .keys()
+      .then(keys => keys.filter(key => !key.endsWith(version)))
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+  );
+});
 
-  event.respondWith( promise );
-} );
+}
+else {
+    console.log('Boo! Workbox didnt load ');
+}
+`
+
+async function handleRequest(request) {
+  return new Response(js, {
+    headers: {
+      "content-type": "application/javascript;charset=UTF-8",
+    },
+  })
+}
+
+addEventListener("fetch", event => {
+  return event.respondWith(handleRequest(event.request))
+})
