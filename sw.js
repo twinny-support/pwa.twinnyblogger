9a961463cde5b9fc06bd3b255b4722dc982d8b0e
Twinny Support <twinsxstore@gmail.com>
2022-03-02 00:18:58 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,23 @@
-// Change this to your repository name
-var GHPATH = '/pwa.twinnyblogger';
- 
-// Choose a different app prefix name
-var APP_PREFIX = 'gppwa_';
- 
-// The version of the cache. Every time you change any of the files
-// you need to change this version (version_01, version_02…). 
-// If you don't change the version, the service worker will give your
-// users the old files!
-var VERSION = 'version_00';
- 
-// The files to make available for offline use. make sure to add 
-// others to this list
-var URLS = [    
-  `${GHPATH}/`
-]
+var SLOW_TIME = 3000;
+
+this.addEventListener( 'install', function () {
+  console.log('Installed service worker');
+} );
+
+this.addEventListener( 'fetch', function(event) {
+  var url = event.request.url;
+
+  if ( url.indexOf( 'blocking' ) === -1) {
+  return;
+  }
+
+  var promise = Promise.race( [
+    new Promise( ( resolve, reject) => setTimeout(
+      () => reject( new Response( 'Request killed!' ) ),
+      SLOW_TIME
+    ) ),
+    fetch( event.request ),
+  ] );
+
+  event.respondWith( promise );
+} );
