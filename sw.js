e20e801ea35ef1e8b95a68feb7718ec1362b6ef4
Tokko Blogger <21377149+twinnybloggers@users.noreply.github.com>
2022-05-17 17:11:38 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,43 @@
-addEventListener("fetch", event => { event.respondWith(handleRequest(event)) }) //const BUCKET_NAME = "main" const BUCKET_URL = `https://cdn.statically.io/gh/twinny-support/icon` async function serveAsset(event) { const url = new URL(event.request.url) const cache = caches.default let response = await cache.match(event.request) if (!response) { response = await fetch(`${BUCKET_URL}${url.pathname}`) const headers = { "cache-control": "public, max-age=14400" } response = new Response(response.body, { ...response, headers }) event.waitUntil(cache.put(event.request, response.clone())) } return response } async function handleRequest(event) { if (event.request.method === "GET") { let response = await serveAsset(event) if (response.status > 399) { response = new Response(response.statusText, { status: response.status }) } return response } else { return new Response("Method not allowed", { status: 405 }) } }
+// Files to cache 
+ const cacheName = 'twinnybloggers'; 
+ const appShellFiles = [ 
+   '/twinnybloggers/pwa.twinnyblogger/', 
+   '/twinnybloggers/pwa.twinnyblogger/app.js', 
+   '/twinnybloggers/pwa.twinnyblogger/icons/icon-32.png', 
+   '/twinnybloggers/pwa.twinnyblogger/icons/icon-64.png', 
+   '/twinnybloggers/pwa.twinnyblogger/icon-96.png', 
+   '/twinnybloggers/pwa.twinnyblogger/icons/icon-128.png', 
+   '/twinnybloggers/pwa.twinnyblogger/icons/icon-168.png', 
+   '/twinnybloggers/pwa.twinnyblogger/icons/icon-192.png', 
+   '/twinnybloggers/pwa.twinnyblogger/icons/icon-256.png', 
+   '/twinnybloggers/pwa.twinnyblogger/icons/icon-512.png', 
+ ]; 
+ const gamesImages = []; 
+ for (let i = 0; i < games.length; i++) { 
+   gamesImages.push(`data/img/${games[i].slug}.jpg`); 
+ } 
+ const contentToCache = appShellFiles.concat(gamesImages); 
+  
+ // Installing Service Worker 
+ self.addEventListener('install', (e) => { 
+   console.log('[Service Worker] Install'); 
+   e.waitUntil((async () => { 
+     const cache = await caches.open(cacheName); 
+     console.log('[Service Worker] Caching all: app shell and content'); 
+     await cache.addAll(contentToCache); 
+   })()); 
+ }); 
+  
+ // Fetching content using Service Worker 
+ self.addEventListener('fetch', (e) => { 
+   e.respondWith((async () => { 
+     const r = await caches.match(e.request); 
+     console.log(`[Service Worker] Fetching resource: ${e.request.url}`); 
+     if (r) return r; 
+     const response = await fetch(e.request); 
+     const cache = await caches.open(cacheName); 
+     console.log(`[Service Worker] Caching new resource: ${e.request.url}`); 
+     cache.put(e.request, response.clone()); 
+     return response; 
+   })()); 
+ });
